374e97331a65aac3fbf74a972e88a8f7603e2ab4
ilaoming <jesus.rios2609@gmail.com>
2021-12-12 22:21:41 -0500

    12/12/2021 Add flash and session

--- app.js
+++ app.js
@@ -1,28 +1,42 @@
 const express = require('express')
-const app = express();
 const bodyParser = require('body-parser')
-app.set('view engine','ejs');
+const session = require('express-session')
+const flash = require('express-flash')
+const app = express();
 
+app.set('view engine','ejs');
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({ secret: 'token-muy-secreto', resave: true, saveUninitialized: true }));
+app.use(flash())
 
 
 
 
 
 app.get('/', function(req, res) {
-    res.render('index');
+    res.render('index',{ error: peticion.flash('error') });
   });
 
   app.get('/home', function(req, res) {
-    res.render('home');
+    res.render('home',{ user: req.session.user });
+  });
+
+  app.get('/detalle', function(req, res) {
+    res.render('detalle',{ user: req.session.user });
+  });
+
+  app.get('/salir', function(req, res) {
+      req.session.destroy();
+    res.redirect('/')
   });
 
   app.post('/autenticar', function(req, res) {
     if (req.body.user == "laoming" && req.body.pass == "5702471") {
+        req.session.user = req.body.user
         res.redirect('/home')
     }else{
-        console.log("Datos invalidos");
+        req.flash('error', "Datos incorrectos")
         res.redirect('/')
     }
   });
